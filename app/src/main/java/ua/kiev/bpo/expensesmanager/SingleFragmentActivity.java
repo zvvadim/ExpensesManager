785d70d2d85ea7785ba83dad65af2825c474c11a
Vadym Zaichenko <zvvadim@yandex.ru>
2015-10-21 20:11:06 +0300

    New project

---
+++ app/src/main/java/ua/kiev/bpo/expensesmanager/SingleFragmentActivity.java
@@ -0,0 +1,13 @@
+package ua.kiev.bpo.expensesmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingleFragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+    }
+}
