04888f4468dee5095bc87d2cffa75a4aaf136a9b
Vadym Zaichenko <zvvadim@yandex.ru>
2016-02-10 22:27:56 +0200

    Calculator works. Started to add togle actionbar

--- app/src/main/java/ua/kiev/bpo/expensesmanager/ExpensesManager.java
+++ app/src/main/java/ua/kiev/bpo/expensesmanager/ExpensesManager.java
@@ -7,47 +7,138 @@ import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.app.Activity;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.Menu;
 import android.view.View;
+import android.view.View.OnLongClickListener;
+import android.view.ViewAnimationUtils;
+import android.view.ViewGroupOverlay;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
+import android.widget.TextView;
 
+import ua.kiev.bpo.expensesmanager.ExpensesManagerEditText.OnTextSizeChangeListener;
 import ua.kiev.bpo.expensesmanager.ExpensesManagerExpressionEvaluator.EvaluateCallback;
 
-public class ExpensesManager extends Activity implements EvaluateCallback{
-
-    private static final String NAME = ExpensesManager.class.getName();
-
-    // instance state keys
-    private static final String KEY_CURRENT_STATE = NAME + "_currentState";
-    private static final String KEY_CURRENT_EXPRESSION = NAME + "_currentExpression";
+public class ExpensesManager extends Activity implements EvaluateCallback,
+        OnTextSizeChangeListener, OnLongClickListener {
 
     /**
      * Constant for an invalid resource id.
      */
     public static final int INVALID_RES_ID = -1;
-
-    private enum ExpensesManagerState {INPUT, EVALUATE, RESULT, ERROR};
+    private static final String NAME = ExpensesManager.class.getName();
+    // instance state keys
+    private static final String KEY_CURRENT_STATE = NAME + "_currentState";
+    private static final String KEY_CURRENT_EXPRESSION = NAME + "_currentExpression";
 
     private View mDisplayView;
     private ExpensesManagerEditText mFormulaEditText;
     private ExpensesManagerEditText mResultEditText;
+    private ViewPager mPadViewPager;
+    private View mDeleteButton;
+    private View mClearButton;
+    private View mEqualButton;
 
     private ExpensesManagerState mCurrentState;
     private ExpensesManagerExpressionTokenizer mTokenizer;
+    private final Editable.Factory mFormulaEditableFactory = new Editable.Factory() {
+        @Override
+        public Editable newEditable(CharSequence source) {
+            final boolean isEdited = mCurrentState == ExpensesManagerState.INPUT
+                    || mCurrentState == ExpensesManagerState.ERROR;
+            return new ExpensesManagerExpressionBuilder(source, mTokenizer, isEdited);
+        }
+    };
     private ExpensesManagerExpressionEvaluator mEvaluator;
+    private final TextWatcher mFormulaTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            setState(ExpensesManagerState.INPUT);
+            mEvaluator.evaluate(s, ExpensesManager.this);
+        }
+    };
+    private final View.OnKeyListener mFormulaOnKeyListener = new View.OnKeyListener() {
+        @Override
+        public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_NUMPAD_ENTER:
+                case KeyEvent.KEYCODE_ENTER:
+                    if (keyEvent.getAction() == KeyEvent.ACTION_UP) {
+                        onEquals();
+                    }
+                    // ignore all other actions
+                    return true;
+            }
+            return false;
+        }
+    };
     private Animator mCurrentAnimator;
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator_port);
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R
+                .string.open_drawer, R.string.close_drawer) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        //getActionBar().setHomeButtonEnabled(true);
+
+
+
         mDisplayView = (View) findViewById(R.id.display);
         mFormulaEditText = (ExpensesManagerEditText) findViewById(R.id.formula);
         mResultEditText = (ExpensesManagerEditText) findViewById(R.id.result);
+        mPadViewPager = (ViewPager) findViewById(R.id.pad_pager);
+        mDeleteButton = findViewById(R.id.del);
+        mClearButton = findViewById(R.id.clr);
+
+        mEqualButton = findViewById(R.id.pad_numeric).findViewById(R.id.eq);
+        if (mEqualButton == null || mEqualButton.getVisibility() != View.VISIBLE) {
+            mEqualButton = findViewById(R.id.pad_operator).findViewById(R.id.eq);
+        }
 
         mTokenizer = new ExpensesManagerExpressionTokenizer(this);
         mEvaluator = new ExpensesManagerExpressionEvaluator(mTokenizer);
@@ -55,38 +146,75 @@ public class ExpensesManager extends Activity implements EvaluateCallback{
         savedInstanceState = savedInstanceState == null ? Bundle.EMPTY : savedInstanceState;
         setState(ExpensesManagerState.values()[savedInstanceState.getInt(KEY_CURRENT_STATE,
                 ExpensesManagerState
-                .INPUT.ordinal())]);
+                        .INPUT.ordinal())]);
+        mFormulaEditText.setText(mTokenizer.getLocalizedExpression(
+                savedInstanceState.getString(KEY_CURRENT_EXPRESSION, "")));
+        mEvaluator.evaluate(mFormulaEditText.getText(), this);
+
+        mFormulaEditText.setEditableFactory(mFormulaEditableFactory);
+        mFormulaEditText.addTextChangedListener(mFormulaTextWatcher);
+        mFormulaEditText.setOnKeyListener(mFormulaOnKeyListener);
+        mFormulaEditText.setOnTextSizeChangeListener(this);
+        mDeleteButton.setOnLongClickListener(this);
+
     }
 
-    private void setState(ExpensesManagerState state){
-        if (mCurrentState != state){
+    private void setState(ExpensesManagerState state) {
+        if (mCurrentState != state) {
             mCurrentState = state;
         }
     }
 
     @Override
+    public boolean onLongClick(View v) {
+        if (v.getId() == R.id.del) {
+            onClear();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
     public void onEvaluate(String expr, String result, int errorResourceId) {
-        if (mCurrentState == ExpensesManagerState.INPUT){
+        if (mCurrentState == ExpensesManagerState.INPUT) {
             mResultEditText.setText(result);
-        } else if (errorResourceId != INVALID_RES_ID){
+        } else if (errorResourceId != INVALID_RES_ID) {
             onError(errorResourceId);
-        } else if (!TextUtils.isEmpty(result)){
+        } else if (!TextUtils.isEmpty(result)) {
             onResult(result);
-        } else if (mCurrentState == ExpensesManagerState.EVALUATE){
+        } else if (mCurrentState == ExpensesManagerState.EVALUATE) {
             setState(ExpensesManagerState.INPUT);
         }
 
         mFormulaEditText.requestFocus();
     }
 
-    private void onError(final int errorResourceId){
-        if (mCurrentState == ExpensesManagerState.EVALUATE){
-            mResultEditText.setText(errorResourceId);
+    @Override
+    public void onTextSizeChanged(TextView textView, float oldSize) {
+        if (mCurrentState != ExpensesManagerState.INPUT) {
             return;
         }
+
+        // Calculate the values needed to perform the scale and translation animations,
+        // maintaining the same apparent baseline for the displayed text.
+        final float textScale = oldSize / textView.getTextSize();
+        final float translationX = (1.0f - textScale) *
+                (textView.getWidth() / 2.0f - textView.getPaddingEnd());
+        final float translationY = (1.0f - textScale) *
+                (textView.getHeight() / 2.0f - textView.getPaddingBottom());
+
+        final AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(
+                ObjectAnimator.ofFloat(textView, View.SCALE_X, textScale, 1.0f),
+                ObjectAnimator.ofFloat(textView, View.SCALE_Y, textScale, 1.0f),
+                ObjectAnimator.ofFloat(textView, View.TRANSLATION_X, translationX, 0.0f),
+                ObjectAnimator.ofFloat(textView, View.TRANSLATION_Y, translationY, 0.0f));
+        animatorSet.setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
+        animatorSet.setInterpolator(new AccelerateDecelerateInterpolator());
+        animatorSet.start();
     }
 
-    private void onResult(final String result){
+    private void onResult(final String result) {
         // Calculate the values needed to perform the scale and translation animations,
         // accounting for how the scale will affect the final position of the text.
         final float resultScale =
@@ -166,20 +294,107 @@ public class ExpensesManager extends Activity implements EvaluateCallback{
         }
     }
 
-    private void onEquals(){
-        if (mCurrentState == ExpensesManagerState.INPUT){
+    private void onEquals() {
+        if (mCurrentState == ExpensesManagerState.INPUT) {
             setState(ExpensesManagerState.EVALUATE);
-            mEvaluator.evaluate(mFormulaEditText.getText(),this);
+            mEvaluator.evaluate(mFormulaEditText.getText(), this);
         }
     }
 
-    private void onDelete(){
-
+    private void onDelete() {
+        // Delete works like backspace; remove the last character from the expression.
+        final Editable formulaText = mFormulaEditText.getEditableText();
+        final int formulaLength = formulaText.length();
+        if (formulaLength > 0) {
+            formulaText.delete(formulaLength - 1, formulaLength);
+        }
     }
 
-    private void onClear(){
+    private void reveal(View sourceView, int colorRes, Animator.AnimatorListener listener) {
+        final ViewGroupOverlay groupOverlay =
+                (ViewGroupOverlay) getWindow().getDecorView().getOverlay();
 
+        final Rect displayRect = new Rect();
+        mDisplayView.getGlobalVisibleRect(displayRect);
+
+        // Make reveal cover the display and status bar.
+        final View revealView = new View(this);
+        revealView.setBottom(displayRect.bottom);
+        revealView.setLeft(displayRect.left);
+        revealView.setRight(displayRect.right);
+        revealView.setBackgroundColor(getResources().getColor(colorRes));
+        groupOverlay.add(revealView);
+
+        final int[] clearLocation = new int[2];
+        sourceView.getLocationInWindow(clearLocation);
+        clearLocation[0] += sourceView.getWidth() / 2;
+        clearLocation[1] += sourceView.getHeight() / 2;
+
+        final int revealCenterX = clearLocation[0] - revealView.getLeft();
+        final int revealCenterY = clearLocation[1] - revealView.getTop();
+
+        final double x1_2 = Math.pow(revealView.getLeft() - revealCenterX, 2);
+        final double x2_2 = Math.pow(revealView.getRight() - revealCenterX, 2);
+        final double y_2 = Math.pow(revealView.getTop() - revealCenterY, 2);
+        final float revealRadius = (float) Math.max(Math.sqrt(x1_2 + y_2), Math.sqrt(x2_2 + y_2));
+
+        final Animator revealAnimator =
+                ViewAnimationUtils.createCircularReveal(revealView,
+                        revealCenterX, revealCenterY, 0.0f, revealRadius);
+        revealAnimator.setDuration(
+                getResources().getInteger(android.R.integer.config_longAnimTime));
+
+        final Animator alphaAnimator = ObjectAnimator.ofFloat(revealView, View.ALPHA, 0.0f);
+        alphaAnimator.setDuration(
+                getResources().getInteger(android.R.integer.config_mediumAnimTime));
+        alphaAnimator.addListener(listener);
+
+        final AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.play(revealAnimator).before(alphaAnimator);
+        animatorSet.setInterpolator(new AccelerateDecelerateInterpolator());
+        animatorSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                groupOverlay.remove(revealView);
+                mCurrentAnimator = null;
+            }
+        });
+
+        mCurrentAnimator = animatorSet;
+        animatorSet.start();
     }
+
+    private void onClear() {
+        if (TextUtils.isEmpty(mFormulaEditText.getText())) {
+            return;
+        }
+
+        final View sourceView = mClearButton.getVisibility() == View.VISIBLE
+                ? mClearButton : mDeleteButton;
+        reveal(sourceView, R.color.calculator_accent_color, new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                mFormulaEditText.getEditableText().clear();
+            }
+        });
+    }
+
+    private void onError(final int errorResourceId) {
+        if (mCurrentState != ExpensesManagerState.EVALUATE) {
+            // Only animate error on evaluate.
+            mResultEditText.setText(errorResourceId);
+            return;
+        }
+
+        reveal(mEqualButton, R.color.calculator_error_color, new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                setState(ExpensesManagerState.ERROR);
+                mResultEditText.setText(errorResourceId);
+            }
+        });
+    }
+
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -204,4 +419,6 @@ public class ExpensesManager extends Activity implements EvaluateCallback{
             mCurrentAnimator.end();
         }
     }
+
+    private enum ExpensesManagerState {INPUT, EVALUATE, RESULT, ERROR}
 }
